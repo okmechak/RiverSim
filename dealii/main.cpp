79191add2cc1c76ef999cb954b3f648778831025
Oleg Kmechak <ofcrashbash@gmail.com>
2018-09-29 23:56:29 +0200

    Add deal project and its build

---
+++ dealii/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "river_sim.hpp"
+using namespace std;
+
+int main(int argc, char const **argv)
+{
+    cout << "river simulation" << endl;    
+    return 0;
+}
---
+++ dealii_build/CMakeFiles/river_sim.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/river_sim.dir/main.cpp.o"
+  "river_sim.pdb"
+  "river_sim"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/river_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
