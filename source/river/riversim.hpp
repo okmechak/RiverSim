da4e3a98deec71b49db71e4c78af08687b5f064d
Oleg Kmechak <ofcrashbash@gmail.com>
2018-11-05 12:49:37 +0100

    add dof and fe headers

--- source/river/riversim.hpp
+++ source/river/riversim.hpp
@@ -6,6 +6,10 @@
 #include <deal.II/grid/manifold_lib.h>
 #include <deal.II/grid/grid_out.h>
 #include <deal.II/grid/grid_in.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_tools.h>
+#include <deal.II/dofs/dof_renumbering.h>
+#include <deal.II/fe/fe_q.h>
 
 #include <iostream>
 #include <fstream>
