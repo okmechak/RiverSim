b824b9fc0d62007ad0b594bfb7017cc83963ba6a
Oleg Kmechak <ofcrashbash@gmail.com>
2019-03-26 01:16:43 +0100

    [MAJOR] new implementation of rivers growth

--- source/main.cpp
+++ source/main.cpp
@@ -12,13 +12,14 @@
 #include "riversim.hpp"
 
 using namespace River;
+using namespace std;
 ///Entry point of RiverSim program.
 /** It glues all together user inputs(commands) and inner objects to run simulation */
 int main(int argc, char *argv[])
 {
     /**
-            Program options
-         */
+        Program options
+    */
     auto vm = process_program_options(argc, argv);
 
     if (vm.count("help") || vm.count("version"))
@@ -27,141 +28,61 @@ int main(int argc, char *argv[])
     if (!vm.count("supprchess-signature"))
         print_ascii_signature();
 
-    /**
-            Definition of all main objects used in project
-        */
-    ///Model parameters
-    River::Model model;
-    model.ds = vm["ds"].as<double>();
-    model.dx = vm["dx"].as<double>();
-    model.eps = vm["eps"].as<double>();
+    if(!vm["dev-mode"].as<bool>())
+        ForwardRiverEvolution(vm);
 
-    tethex::Mesh meshio;
-    River::Geometry geom;
-    River::Triangle tr;
-    River::Solver sim;
-    
+    auto river_boundary_id = 3;
+    auto boundary_ids = vector<int>{0, 1, 2, river_boundary_id};
+    auto region_size = vector<double>{1, 1};
+    auto sources_x_coord = vector<double>{0.1};
+    auto sources_id = vector<int>{1};
 
-    /**
-            Physical Model Object
-        */
+    Model mdl;
+    tethex::Mesh border_mesh;
+    Border border(border_mesh);
+    border.eps = mdl.eps;
+    border.MakeRectangular(
+        region_size, 
+        boundary_ids,
+        sources_x_coord,
+        sources_id);
 
+    Tree tr(border.GetSourcesPoint(), border.GetSourcesNormalAngle(), border.GetSourcesId());
+    //tr.GetBranch(sources_id.at(0)).AddPoint(Polar{0.1, 0});
 
-    /*
-                Geometry Object
-        */
-    switch(vm["geom-type"].as<int>())
-    {   
-        case 0: 
-            geom = SimpleGeo::Box(model);
-            break;
-        case 1:
-            geom = SimpleGeo::SingleTip(model, 3);
-            break;
-        case 2:
-            geom = SimpleGeo::CustomRiverTree(model, 3);
-            break;
-    }
+    //Loop
+    for(unsigned i = 0; i < vm["number-of-steps"].as<unsigned>(); ++i){
+        auto mesh = BoundaryGenerator(mdl, tr, border, river_boundary_id);
 
-    /**
-         *****Main
-         *****Program
-         *****Cycle
-         **/
-    River::Gmsh Gmsh;
-    for(int i = 0; i < vm["steps"].as<int>(); ++i)
-    {
-        cout << endl 
-             << "==========" << endl;
-        cout << "step - " << i << endl;
-        cout << "==========" << endl << endl;
+        Triangle tria;
+        tria.AreaConstrain = tria.ConstrainAngle = true;
+        tria.MaxTriaArea = vm["mesh-max-area"].as<double>();
+        tria.MinAngle = vm["mesh-min-angle"].as<double>();
+        tria.generate(mesh);
+        mesh.convert();
+        mesh.write(vm["output-mesh"].as<string>());
 
-        geom.InitiateMesh(meshio);
+        //Simulation
+        //Deal.II library
+        River::Solver sim;
+        sim.numOfRefinments = vm["ref-num"].as<int>();
+        sim.SetBoundaryRegionValue(boundary_ids, 0.);
+        sim.OpenMesh(vm["output-mesh"].as<string>());
+        sim.run(0);
+        auto tip_point = tr.GetBranch(sources_id.at(0)).TipPoint();
+        auto tip_angle = tr.GetBranch(sources_id.at(0)).TipAngle();
+        auto series_params = sim.integrate(tip_point, tip_angle);
 
-        /**
-                        Mesh Generation
-                   */
-        
-        if(vm["mesh"].as<bool>())
-        {
-            if(vm["use-gmsh"].as<bool>())
-            {
-                Gmsh.init();
-                Gmsh.generate(meshio);
-                Gmsh.write(vm["output-mesh"].as<string>());
-            }
-            else
-            {
-                //options
-                tr.Verbose = vm["verbose"].as<bool>();
-                tr.Quite = vm["quiet"].as<bool>();
-                tr.AreaConstrain = tr.ConstrainAngle = true;
-                tr.MaxTriaArea = vm["mesh-max-area"].as<double>();
-                tr.MinAngle = vm["mesh-min-angle"].as<double>();
-                //generate mesh
-                tr.generate(meshio);
-                //convert triangles to quadrangles
-                meshio.convert();
-                meshio.write(vm["output-mesh"].as<string>());
-            }
-        }
-        else
-            meshio.write(vm["output-mesh"].as<string>());
+        cout << "Volume - " << series_params.at(0) << endl;
 
-        if(vm["verbose"].as<bool>())
-            meshio.info();
-
-
-        /**
-                        Solver
-                   */
-        if(vm["simulate"].as<bool>())
-        {
-            //Deal.II library
-            sim.numOfRefinments = vm["ref-num"].as<int>();
-            sim.SetBoundaryRegionValue({Geometry::Markers::Bottom, Geometry::Markers::River}, 0.);
-            sim.SetBoundaryRegionValue({Geometry::Markers::Top}, 1.);
-            //sim.SetMesh(meshio);
-            sim.OpenMesh(vm["output-mesh"].as<string>());
-            sim.run(i);
-
-            for(auto p: geom.GetTipPoints())
-            {
-                cout << endl << endl;
-                cout << p.first << ", angle : " << p.second << endl;
-                auto series_params = sim.integrate(p.first, p.second);
-
-                for(auto a: series_params)
-                    cout << a << " ";
-                cout << endl;
-                auto newPoint = model.next_point(series_params);
-                newPoint.branchId = p.first.branchId;
-                cout << newPoint.branchId << endl << flush;
-                geom.addPolar(newPoint);
-                cout << endl << endl;
-            }
-            //freeup resources
-            sim.clear();
-        }
-        
-        /**
-                        Freeup resources
-                   */
-        geom.clear();
-        meshio.clean();
-        if(vm["use-gmsh"].as<bool>())
-            Gmsh.clear();
-
+        cout << "Next Point" << endl;
+        cout << Model::next_point(series_params) << endl;
+        tr.AddPoints({Model::next_point(series_params)}, {sources_id.at(0)});
     }
     
-    /**
-                   Visualisation
-         */
-    if(vm["visualise"].as<bool>())
-    {
-        Gmsh.open(vm["output-mesh"].as<string>());
-        Gmsh.start_ui();
-    }
+    Gmsh gmsh;
+    gmsh.open(vm["output-mesh"].as<string>());
+    gmsh.start_ui();
     
     return 0;
 }
