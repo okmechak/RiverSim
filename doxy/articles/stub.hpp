ae054c35f64c7345641c3e9302962476ccad18f8
Oleg Kmechak <ofcrashbash@gmail.com>
2019-03-28 15:49:52 +0100

    documentation modules and logo

---
+++ doxy/articles/stub.hpp
@@ -0,0 +1,28 @@
+/**
+ * \ingroup A
+ */
+extern int VarInA;
+
+/**
+ * \defgroup IntVariables Global integer variables
+ * @{
+ */
+
+/** an integer variable */
+extern int IntegerVariable;
+
+/**@}*/
+
+
+
+/**
+ * \defgroup Variables Global variables
+ */
+/**@{*/
+
+/** a variable in group A */
+int VarInA;
+
+int IntegerVariable;
+
+/**@}*/
--- doxy/articles/stub.md
+++
@@ -1,4 +0,0 @@
-\addtogroup stub_label_name
-@{
-# SOME TEXT WILL BE GOING HERE
-@}
