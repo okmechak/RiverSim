c2dae155064b9910b309d94867971eb75852424c
Oleg Kmechak <ofcrashbash@gmail.com>
2019-03-28 22:21:29 +0100

    [MAJOR] implement base vector and weigth function in same way as it was in freefem

--- source/river/physmodel.hpp
+++ source/river/physmodel.hpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cmath>
 #include <functional>
+#include <complex>
+#include <math.h>
 
 #include "common.hpp"
 
@@ -14,10 +16,10 @@ namespace River
     class Model
     {   
         public: 
-            double eps = 1e-4;
             /*
-                                Different parameters that is used in river simulation
-                            */
+                Different parameters that is used in river simulation
+            */
+            double eps = 1e-4;
             enum class Method {Royal, Simple, Min, Integral};
             double ac = 0.;
             static constexpr double ds = 0.01;
@@ -27,13 +29,28 @@ namespace River
             static constexpr double Rmin = 0.0000000001;
             static constexpr double Rmax = 0.01;
             double biffurcationThreshold = 0.1;
-            
+            static constexpr int exponant = 2;
+
             bool q_biffurcate(double a1, double a2, double a3);
             static Polar next_point(vector<double> series_params);
-
+ 
             static function<double(double, double)> Gain(int index);
             
-        private:
+        //private: FIXME: how to test private members???
+            static double BaseVector(int nf, complex<double> zf)
+            {
+                if( (nf % 2) == 0)
+                    return -imag(pow(zf, nf/2.));
+                else
+                    return real(pow(zf, nf/2.));
+                
+            }
+
+            static double WeightFunction(double r)
+            {
+                return exp(-pow(r/Rmax, exponant));
+            }
+
 
 
 
