47883b37e055fcb5443411c83d8770e232843a4e
Oleg Kmechak <ofcrashbash@gmail.com>
2019-04-09 18:38:45 +0200

    Implement class for program timing

--- source/river/utilities.hpp
+++ source/river/utilities.hpp
@@ -22,6 +22,8 @@
 #pragma once
 
 #include "cxxopts.hpp"
+#include <chrono>
+#include <ctime>
 
 using namespace std;
 
@@ -36,5 +38,70 @@ namespace River{
 
     ///Processing of program options.
     cxxopts::ParseResult process_program_options(int argc, char* argv[]);
-    
+
+    ///FIXME time isn't in seconds or is wrong
+    ///Timing
+    class Timing
+    {
+        public:
+            
+            ///Default Constructor, save time of creation.
+            Timing()
+            {
+                last_time_point = chrono::system_clock::now();
+            }
+
+            ///Return current date string.
+            string CurrentDate()
+            {   
+                auto now = chrono::system_clock::now();
+                auto t_time = chrono::system_clock::to_time_t(now);
+                return ctime(&t_time);
+            }
+
+            ///Return date of object creation string.
+            string CreationtDate()
+            {   
+                auto t_time = chrono::system_clock::to_time_t(last_time_point);
+                return ctime(&t_time);
+            }
+
+            ///Returns elapsed time, and save it into vector.
+            double Record()
+            {
+                records.push_back(
+                    (chrono::system_clock::now() 
+                    - last_time_point).count()
+                );
+                if(flag)
+                {
+                    //quickfix
+                    records.erase(begin(records));
+                    flag = false;
+                }
+
+                return records.back();
+            }
+
+
+            ///Return total time of simmulation.
+            double Total()
+            {
+                double sum = 0;
+                for(auto el: records)
+                    sum += el;
+                return sum;
+            }
+
+            ///Vector that holds all times between Record() calls.
+            vector<double> records;
+        
+        private:
+
+            //quickfix
+            bool flag = true;
+
+            ///Stores last time from callig Record function.
+            chrono::system_clock::time_point last_time_point;
+    };
 }
