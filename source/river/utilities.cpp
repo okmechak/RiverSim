b6caaf709cf6b43a343a137445c3a64bf89a7112
Oleg Kmechak <ofcrashbash@gmail.com>
2019-04-15 11:41:21 +0200

    model default value upd

--- source/river/utilities.cpp
+++ source/river/utilities.cpp
@@ -85,7 +85,7 @@ namespace River
         ("eta", "Power of a1^eta.", value<double>()->default_value("0"))
         ("biffurcation-type", "Biffurcation method type. 0 - a3/a1 > biffurcation_threshold, 1 - a1 > biffurcation_threshold, 2 - combines both conditions, 3 - no biffurcation at all.", value<int>()->default_value("2"))
         ("b,biffurcation-threshold", "Biffuraction threshold for a(3)/a(1) > kcrit", value<double>()->default_value("-0.1"))
-        ("biffurcation-threshold-2", "Biffuraction threshold for a(1) > kcrit", value<double>()->default_value("0.001"))
+        ("biffurcation-threshold-2", "Biffuraction threshold for a(1) > kcrit", value<double>()->default_value("0.002"))
         ("biffurcation-angle", "Biffurcation angle between branches. default Pi/5 or Pi/10?? FIXME", value<double>()->default_value("0.62831853071795864"))
         ("growth-type", "0 - arctan(a2/a1), 1 - {dx, dy}.", value<int>()->default_value("0"))
         ("growth-threshold", "Growth of branch will be done only if a1 > growth-threshold.", value<double>()->default_value("0."))
