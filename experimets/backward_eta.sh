be3d4d35c38ac2a7dbfa7844ec4cce4e00f3a147
Oleg Kmechak <ofcrashbash@gmail.com>
2019-05-31 23:39:39 +0200

    fcuk

--- experimets/backward_eta.sh
+++ experimets/backward_eta.sh
@@ -1,18 +1,17 @@
-echo pwd
-
-./source/riversim -t 1 -n 70 --eta=0.1 -o eta_00 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=0.1 -o eta_01 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=0.2 -o eta_02 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=0.3 -o eta_03 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=0.4 -o eta_04 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=0.5 -o eta_05 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=0.6 -o eta_06 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=0.7 -o eta_07 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=0.8 -o eta_08 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=0.9 -o eta_09 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=1.0 -o eta_10 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=1.1 -o eta_11 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=1.2 -o eta_12 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=1.3 -o eta_13 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=1.4 -o eta_14 ./test_tree.json
-./source/riversim -t 1 -n 70 --eta=1.5 -o eta_15 ./test_tree.json
+#!/bin/bash
+./source/riversim -t 1 -n 70 --eta=0.0 -o eta_00 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=0.1 -o eta_01 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=0.2 -o eta_02 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=0.3 -o eta_03 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=0.4 -o eta_04 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=0.5 -o eta_05 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=0.6 -o eta_06 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=0.7 -o eta_07 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=0.8 -o eta_08 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=0.9 -o eta_09 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=1.0 -o eta_10 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=1.1 -o eta_11 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=1.2 -o eta_12 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=1.3 -o eta_13 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=1.4 -o eta_14 /home/okmechak/dev/build/tree_test.json
+./source/riversim -t 1 -n 70 --eta=1.5 -o eta_15 /home/okmechak/dev/build/tree_test.json
