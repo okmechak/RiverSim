25ee7e881597fc92a7192f6bd9fbe845fe02735c
Oleg Kmechak <ofcrashbash@gmail.com>
2019-07-29 04:35:48 +0300

    add multithreadind and other features to pl script

--- experimets/parameter_lookup.sh
+++ experimets/parameter_lookup.sh
@@ -1,94 +1,222 @@
 #!/bin/bash
 
-PROGRAM_NAME=none
-if [ -z "$1" ]; then
-    echo "Please specifie as first parameter path to the riversim program."
-    exit 1
-else
-    PROGRAM_NAME=$1
-fi
-echo "$PROGRAM_NAME"
+#----------------------------
+#function definitions section
+#----------------------------
 
-version=$($PROGRAM_NAME -v)
-required_version="v2.4.1"
-index=$(expr index "$version" "$required_version")
-if [ "$index" -eq "0" ];then
-    echo "Your program version is"  "$version" ", but required version is" "$required_version" 
-    read -p "Do you want to continue?Y/N " -n 1 -r
-    echo    # (optional) move to a new line
-    if [[ $REPLY =~ ^[Nn]$ ]];then
-        echo Exit
+function display_help()
+{
+    echo "Usage: $0 [option...] /riversim/program/path(mandatory) /output/path/of/simulation=plookup_{unix_time} number-of-threads=3" >&2
+    echo
+    echo "OPTIONS:"
+    echo "   -h, --help           prints this help"
+    echo
+    echo "POSITIONAL PARAMETERS:"
+    echo "   /riversim/program/path(mandatory) - you should specify location of RiverSim program in file system."
+    echo "   /output/path/of/simulation - output path of simulation result. It also will append unix time in secods."
+    echo "                                by default it is plookup_{unix_time}."
+    echo "   number-of-threads - number of simultaneous simulation running. Default is 3."
+}
+
+function handle_help_option()
+{
+    for param in $@
+    do
+        if [[ "$param" == "--help" || "$param" == "-h" ]]
+        then
+            display_help
+            exit 0
+        fi
+    done
+}
+
+function check_program_location_from_first_param()
+{
+    if [[ ! -x $1 ]]
+    then
+        echo "Please specifie as first parameter path to the riversim program. Currently it is equal to: $1"
         exit 1
     fi
-    echo Continue
+}
+
+function get_number_of_threads()
+{
+    if [ -z $1 ]
+    then
+        echo "3"
+    else
+        echo "$1"
+    fi
+}
+
+function check_output_folder()
+{
+    if [[ ! -d $1 ]]
+    then
+        echo "There is no such folder: $1."
+        read -p "Do you want to create it? Y/N " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+            if ! mkdir $1
+            then
+                echo "Folder $1 can't be created."
+                exit 1
+            else 
+                echo "Folder $1 created successfully."
+            fi
+        else
+            exit 0
+        fi
+    fi
+}
+
+function minimal_version_check()
+{
+    #handling of program version
+    
+    version_str=$1
+    version_str=${version_str:23:5} #select only version numbers
+    
+    if [[ $version_str < $2 ]]
+    then
+        echo "Your program version is $version_str, but required version is $2" 
+        read -p "Do you want to continue?Y/N " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Nn]$ ]]
+        then
+            exit 0
+        fi
+        echo Continue...
+    fi
+}
+
+#MULTITHREADING
+# example from stackoverflow
+# https://stackoverflow.com/questions/6593531/running-a-limited-number-of-child-processes-in-parallel-in-bash
+
+# means: run background processes in a separate processes...
+set -m 
+# execute add_next_task when we receive a child complete signal
+trap add_next_task CHLD 
+
+function add_next_task 
+{
+    # if still tasks to do then add one
+    if [[ $index -lt ${#task_array[*]} ]]
+    # apparently stackoverflow doesn't like bash syntax
+    # the hash in the if is not a comment - rather it's bash awkward way of getting its length
+    then
+        echo "adding task $index"
+        do_task "${task_array[$index]}" "$index" & 
+        # replace the line above with the command you want
+        index=$(($index+1))
+    fi
+}
+
+function do_task {
+    echo -e "\tstarting task index: $2"
+    echo -e "\t\t eval $1"
+    sleep 2
+    echo -e "\tend of task index: $2"
+}
+
+#----------------------------
+#program section
+#----------------------------
+
+#IITIALIZATION
+
+echo "Precision parameter lookup."
+handle_help_option $@
+check_program_location_from_first_param $1
+PROGRAM_NAME=$1 #first parameter should be program path
+
+full_program_version=$($PROGRAM_NAME -v)
+echo $full_program_version
+required_version_str="2.4.1"
+
+minimal_version_check "$full_program_version" "$required_version_str"
+
+if [[ -z "$2" ]] 
+then
+    echo "No output folder specified. Current($PWD) will be used."
+    output_folder="plookup"
+else
+    output_folder=$2
+fi
+output_folder="${output_folder}_$(date +%s)"
+check_output_folder $output_folder
+
+
+
+# Currently next parameters has the biggest impact on simulation accuracy(imho): mesh-min-area, mesh-exp, refinment-radius and stati-refinment-steps.
+# these parameters handles mesh refinement aroud tips points.
+# 1 - lets at firts gradualy increase static-refinment-steps.
+# 2 - than gradualy increase refinment-radius
+# 3 - than gradualy increase mesh-min-area
+# and the last one is combination of all above
+# 4 - all parameters increas
+
+
+#MAIN CYCLE
+# creation of task array
+
+#some default values
+simulation_type=2 #development case only values around tip
+mesh_min_area_value=1e-6 
+file_prefix=${output_folder}"/simdata"
+
+task_index=0
+
+
+#static-refinment-steps cycle
+for st_ref_step_val in $(seq 1 7) #static-refinment-steps cycle
+do
+    task_array[$task_index]="$PROGRAM_NAME -V --suppress-signature -t $simulation_type -o ${file_prefix}_st_ref_steps_${st_ref_step_val} --mesh-min-area=$mesh_min_area_value --static-refinment-steps $st_ref_step_val"
+    task_index=$(($task_index+1))
+done
+
+#refinment-radius cycle
+for ref_rad_value in $(seq 0.02 0.02 0.8) #static-refinment-steps cycle
+do
+    task_array[$task_index]="$PROGRAM_NAME -V --suppress-signature -t $simulation_type -o ${file_prefix}_ref_rad_value_${ref_rad_value} --mesh-min-area=$mesh_min_area_value --refinment-radius=$ref_rad_value"
+    task_index=$(($task_index+1))
+done
+
+#mesh-min-area cycle
+for mesh_min_area_val in 1e-6 3e-7 1e-7 3e-8 1e-8 3e-9 1e-9 3e-10 1e-10  #static-refinment-steps cycle
+do
+    task_array[$task_index]="$PROGRAM_NAME -V --suppress-signature -t $simulation_type -o ${file_prefix}_mesh_min_area_val_${mesh_min_area_val} --mesh-min-area=$mesh_min_area_val"
+    task_index=$(($task_index+1))
+done
+
+#mesh-exp cycle
+for mesh_exp_val in $(seq 1 2 30)  #static-refinment-steps cycle
+do
+    task_array[$task_index]="$PROGRAM_NAME -V --suppress-signature -t $simulation_type -o ${file_prefix}_mesh_min_area_val_${mesh_min_area_val} --mesh-min-area=$mesh_min_area_val"
+    task_index=$(($task_index+1))
+done
+
+# end of creation task_array
+# now we have run all those tasks
+
+if [[ ! ${#task_array[*]} -eq 0 ]]
+then
+    index=0
+    max_tasks=$(get_number_of_threads $3)
+    echo "max tasks count: $max_tasks"
+
+    # add initial set of tasks
+    echo "Initial add"
+    while [[ $index -lt $max_tasks ]]
+    do
+        add_next_task
+    done
+    wait # wait for all tasks to complete
+else
+    echo "Task array: $task_array - is empty"
+    exit 1
 fi
 
-
-echo "v2.4.1 test"
-echo "currently next parameters has the biggest impact on simulation: mesh-exp, refinment-radius and stati-refinment-steps."
-echo "these parameters handles mesh refinement aroud tips points."
-echo "1 - lets at firts gradualy increase static-refinment-steps."
-echo "2 - than gradualy increase refinment-radius"
-echo "3 - than gradualy increase mesh-exp"
-echo "and the last one is combination of all above"
-echo "4 - all parameters increas"
-
-echo ""
-echo ""
-echo ""
-echo "FIRST EXPERIMENT"
-echo "1/7"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o first_lookup_1 --mesh-min-area=1e-6 --static-refinment-steps 1 &
-echo "2/7"
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o first_lookup_2 --mesh-min-area=1e-6 --static-refinment-steps 2 &
-echo "3/7"
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o first_lookup_3 --mesh-min-area=1e-6 --static-refinment-steps 3 &
-echo "4/7"
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o first_lookup_4 --mesh-min-area=1e-6 --static-refinment-steps 4 &
-echo "5/7"
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o first_lookup_5 --mesh-min-area=1e-6 --static-refinment-steps 5 &
-echo "6/7"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o first_lookup_6 --mesh-min-area=1e-6 --static-refinment-steps 6 &
-echo "6/7"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o first_lookup_6 --mesh-min-area=1e-6 --static-refinment-steps 7 &
-
-echo ""
-echo ""
-echo ""
-echo "SECOND EXPERIMENT: Mesh refinement radius"
-echo "1/12"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_1  --mesh-min-area=1e-6 --refinment-radius=0.02 &
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_2  --mesh-min-area=1e-6 --refinment-radius=0.05 &
-echo "3/12"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_3  --mesh-min-area=1e-6 --refinment-radius=0.10 &
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_4  --mesh-min-area=1e-6 --refinment-radius=0.16 &
-echo "5/12"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_5  --mesh-min-area=1e-6 --refinment-radius=0.14 &
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_6  --mesh-min-area=1e-6 --refinment-radius=0.25 &
-echo "7/12"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_7  --mesh-min-area=1e-6 --refinment-radius=0.35 &
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_8  --mesh-min-area=1e-6 --refinment-radius=0.40 &
-echo "9/12"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_9  --mesh-min-area=1e-6 --refinment-radius=0.5 &
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_10 --mesh-min-area=1e-6 --refinment-radius=0.6 &
-echo "11/12"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_11 --mesh-min-area=1e-6 --refinment-radius=0.7 &
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o second_lookup_12 --mesh-min-area=1e-6 --refinment-radius=0.8 &
-
-
-echo ""
-echo ""
-echo ""
-echo "LAST EXPERIMENT"
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o last_lookup_1  --mesh-min-area=1e-6  --refinment-radius=0.02 --static-refinment-steps 1 &    
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_2  --mesh-min-area=1e-6  --refinment-radius=0.04 --static-refinment-steps 2 &    
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_3  --mesh-min-area=1e-6  --refinment-radius=0.08 --static-refinment-steps 3 &    
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_4  --mesh-min-area=1e-6  --refinment-radius=0.10 --static-refinment-steps 4 &    
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_5  --mesh-min-area=1e-6  --refinment-radius=0.12 --static-refinment-steps 5 &    
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_6  --mesh-min-area=1e-6  --refinment-radius=0.14 --static-refinment-steps 6 &    
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_7  --mesh-min-area=1e-6  --refinment-radius=0.16 --static-refinment-steps 7 &    
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_8  --mesh-min-area=1e-6  --refinment-radius=0.18 --static-refinment-steps 8 &    
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_9  --mesh-min-area=1e-6  --refinment-radius=0.20 --static-refinment-steps 9 &   
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_10 --mesh-min-area=1e-6  --refinment-radius=0.32 --static-refinment-steps 9 &   
-nohup $PROGRAM_NAME    --suppress-signature -t 2 -o last_lookup_11 --mesh-min-area=1e-6  --refinment-radius=0.44 --static-refinment-steps 9 &  
-nohup $PROGRAM_NAME -V --suppress-signature -t 2 -o last_lookup_12 --mesh-min-area=1e-6  --refinment-radius=0.56 --static-refinment-steps 9 &   
+echo "Done."
