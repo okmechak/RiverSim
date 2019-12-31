69320f97f68b38fc811f71af34d70c760ea7ea83
Oleg Kmechak <ofcrashbash@gmail.com>
2019-12-31 20:23:27 +0200

    version upd

--- VersionsHistory.hpp
+++ VersionsHistory.hpp
@@ -15,8 +15,15 @@
 
     \section versionHistoryFutureVersion Upcoming Versions
 
-    \subsection v270 2.7.0
+    \subsection v20 2.6.5
 
     + fix of backward simulation
+
+    \subsection v21 2.6.6
+
+    + refactoring
+
+    \subsection v23 3.0.0
+
     + multithreading and performance improve
 */
--- source/river/version.hpp
+++ source/river/version.hpp
@@ -10,4 +10,4 @@
 ///Minor version number
 #define VERSION_MINOR 6
 ///PATCH number
-#define VERSION_PATCH 5
+#define VERSION_PATCH 6
