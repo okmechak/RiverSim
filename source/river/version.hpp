7434aa0c17af8d2afa25dce03017e3531bb08afd
Oleg Kmechak <ofcrashbash@gmail.com>
2020-01-17 01:06:11 +0100

    version upd

--- source/river/version.hpp
+++ source/river/version.hpp
@@ -8,6 +8,6 @@
 ///Major version number
 #define VERSION_MAJOR 2
 ///Minor version number
-#define VERSION_MINOR 6
+#define VERSION_MINOR 8
 ///PATCH number
-#define VERSION_PATCH 6
+#define VERSION_PATCH 7
