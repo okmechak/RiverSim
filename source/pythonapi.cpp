bec85f0b1c98b6871386e81fd8c6436d114a5544
Oleg Kmechak <ofcrashbash@gmail.com>
2020-05-27 00:39:25 +0200

    developed Point python api

--- source/pythonapi.cpp
+++ source/pythonapi.cpp
@@ -3,6 +3,7 @@
 #include "app.hpp"
 
 using namespace boost::python;
+using namespace River;
 
 char const* greet()
 {
@@ -13,8 +14,30 @@ BOOST_PYTHON_MODULE(riversim)
 {
     def("greet", greet);
 
-
     class_<River::Point>("Point")
-        .def(init<double, double>())
+        .def(init<double, double>(args( "x", "y" )))
+        .def(init<const River::Point&>())
+        .def(init<const River::Polar&>())
+        .def("norm", static_cast< double (River::Point::*)() const>( &River::Point::norm ) )
+        //.def("norm", static_cast< double (River::Point::*)(const double, const double) >( &River::Point::norm ), args("x", "y") )
+        //.staticmethod("norm")
+        .def("rotate", &River::Point::rotate, args("phi"), return_value_policy<reference_existing_object>())
+        .def("normalize", &River::Point::normalize, return_value_policy<reference_existing_object>())//return value policy
+        .def("angle", static_cast< double (River::Point::*)() const>( &River::Point::angle ))
+        //.def("angle", static_cast<double (River::Point::*)(double, double) >( &River::Point::angle ), args("x", "y") )
+        //.staticmethod( "angle" )
+        .def("angle", static_cast< double (River::Point::*)(const River::Point &) const>( &River::Point::angle ), args("p") )
+        .def(self + self) 
+        .def(self - self) 
+        .def(self += self)
+        .def(self -= self)
+        .def(self == self) 
+        .def(self * self)
+        .def(self * double())
+        .def(self *= double())
+        .def(self / double())
+        .def(self /= double())
+        .def("__getitem__", &River::Point::operator[])
+        //def("__setitem__", &vector_setitem)
     ;
 }
